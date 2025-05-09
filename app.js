0de24f5ef10d268037889c9f64d2ccd36db3fc57
root <132725436+tingtung46@users.noreply.github.com>
2025-05-09 12:41:28 +0700

    Organize routes and add message detail page

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
 const express = require("express");
 const path = require("node:path");
 const app = express();
-
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
+const indexRoute = require("./routes/indexRoute");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "Mini Message Board", messages: messages });
-});
-
-app.get("/new", (req, res) => {
-  res.render("form");
-});
-
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/new", (req, res) => {
-  const messageText = req.body.message;
-  const messageUser = req.body.author;
-
-  messages.push({ text: messageText, user: messageUser, added: new Date() });
-
-  res.redirect("/");
-});
+app.use("/", indexRoute);
 
 app.listen(3000);
---
+++ routes/indexRoute.js
@@ -0,0 +1,42 @@
+const { Router } = require("express");
+const indexRoute = Router();
+const { v4: uuid } = require("uuid");
+
+const messages = [
+  {
+    id: uuid(),
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    id: uuid(),
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+indexRoute.get("/", (req, res) => {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+indexRoute.get("/new", (req, res) => {
+  res.render("form");
+});
+
+indexRoute.post("/new", (req, res) => {
+  const { message, author } = req.body;
+
+  messages.push({ id: uuid(), text: message, user: author, added: new Date() });
+
+  res.redirect("/");
+});
+
+indexRoute.get("/message/:id", (req, res) => {
+  const id = req.params.id;
+  const message = messages.find((item) => id === item.id);
+
+  res.render("details", { message: message });
+});
+
+module.exports = indexRoute;
