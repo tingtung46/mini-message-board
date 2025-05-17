423ec3033563a9edb93d4e0c6c9f2c94bce19dea
root <132725436+tingtung46@users.noreply.github.com>
2025-05-18 00:43:03 +0700

    Add style on form page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("node:path");
 const app = express();
-const indexRoute = require("./routes/indexRoute");
+const indexRoute = require("./routes/indexRouter");
 const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
