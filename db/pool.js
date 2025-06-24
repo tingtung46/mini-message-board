babca1e4cdf942cda274725fb756e60157f4a85b
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2025-06-24 17:19:47 +0700

    Connect app to Neon pg

---
+++ db/pool.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
+
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query("SELECT version()");
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+
+getPgVersion();
